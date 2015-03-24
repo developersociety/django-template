007813c019f34d1159b8935bda42916559559586
Edvinas Jurevicius <ikonitas@gmail.com>
2015-03-24 13:22:23 +0000

    Add fabfile

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from fabric.api import env, run, cd, roles
+
+env.use_ssh_config = True
+
+env.roledefs = {
+    'web': [
+        '{{ project_name }}@runicaath.blanctools.com'
+    ]
+}
+
+env.home = '/var/www/{{ project_name }}'
+
+
+@roles('web')
+def deploy(mode=None):
+    """Deploy to remote server."""
+
+    with cd(env.home):
+        run('git fetch')
+        run('git pull origin master')
+        run('killall -QUIT uwsgi')
+
