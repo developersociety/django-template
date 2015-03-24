21d93ffef744ab7d1a7a8177078e107a03fd0b2e
Edvinas Jurevicius <ikonitas@gmail.com>
2015-03-24 16:18:03 +0000

    Update fabfile deploy and get_backup functions

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from fabric.api import env, run, cd, roles
+from fabric.api import env, run, cd, roles, local
 
 env.use_ssh_config = True
 
@@ -12,16 +12,73 @@ env.roledefs = {
 
 env.home = '/var/www/{{ project_name }}'
 
+env.requirements = '{}/{}'.format(env.home, 'requirements')
+
+DATABASE_NAME = '{{ project_name }}_django'
+DATABASE_SERVER = 'golestandt.blanctools.com'
+
 
 @roles('web')
 def deploy(force_reload=None):
-    """Deploy to remote server."""
+    """
+    Deploy to remote server steps includes pull repo, migrate, install requirements,
+    collect static.
+
+    How to use it:
+
+    fab deploy or fab deploy:force_reload=True
+    """
 
     with cd(env.home):
         run('git fetch')
         run('git pull origin master')
+
+        with cd(env.requirements):
+            run('pip intall -r production.txt')
+
+        # Migarte database changes.
+        run('python manage.py migrate ' '--settings={{ project_name }}.settings.production')
+        # Collectstatic.
+        run(
+            'python manage.py collectstatic --noinput '
+            '--settings={{ project_name }}.settings.production'
+        )
+
         if force_reload:
             run('killall -TERM uwsgi')
         else:
             run('killall -HUP uwsgi')
+
+
+def get_backup(hostname=None, replace_hostname='127.0.0.1'):
+    """
+    Get remote backup and restore database locally.
+
+    How to run with arguments:
+
+    fab get_backup:hostname=example.com,replace_hostname=192.1.1.1
+
+    """
+
+    # Drop database if exists.
+    local('dropdb --if-exists {}'.format(DATABASE_NAME))
+    # Create new database.
+    local('createdb {}'.format(DATABASE_NAME))
+
+    # Connect to the server and dump database.
+    command = 'ssh -C {} sudo -u postgres pg_dump --no-owner {} |'.format(
+        DATABASE_SERVER, DATABASE_NAME
+    )
+
+    if hostname:
+        # If hostname is passed replace with replace_hostname.
+        command = '{} sed -e "s|{}|{}:8000|g" |'.format(
+            command, hostname, replace_hostname
+        )
+
+    # Restore database.
+    command = "{} psql --single-transaction {}".format(command, DATABASE_NAME)
+
+    local(command)
+
 
