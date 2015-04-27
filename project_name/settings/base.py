8627186888e8699ef8ba40f39ef42ab404ffd4c4
Alex Tomkins <tomkins@darkzone.net>
2015-04-27 14:11:27 +0100

    Move static to root directory, compiled static to htdocs/static

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'htdocs/static')
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '{{ project_name }}/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 
