9ae789948a1556d58351e0c52c84d0807f376287
Alex Tomkins <tomkins@darkzone.net>
2015-09-14 11:44:24 +0100

    Make sure we use django extensions locally

--- project_name/settings/local.py
+++ project_name/settings/local.py
@@ -45,9 +45,10 @@ INTERNAL_IPS = (
     '127.0.0.1',
 )
 
-INSTALLED_APPS.append(
+INSTALLED_APPS += [
     'debug_toolbar',
-)
+    'django_extensions',
+]
 
 MIDDLEWARE_CLASSES = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
