58f6d221f1b99422271d87a7f3c8ae3c6ab80ed1
Alex Tomkins <tomkins@darkzone.net>
2016-03-08 11:32:23 +0000

    Use relative paths for grunt

--- gruntfile.js
+++ gruntfile.js
@@ -1,27 +1,22 @@
 module.exports = function(grunt) {
+    staticDir = './static';
 
-PROJECT_NAME = '{{ project_name }}';
+    cssDir = staticDir + '/css';
+    lessDir = staticDir + '/less';
 
-rootDir = '/var/www/' + PROJECT_NAME;
-staticDir = rootDir + '/static';
+    defaultCss = cssDir + '/styles.css';
+    defaultCssMin = cssDir + '/styles.min.css';
+    defaultLess = lessDir + '/styles.less';
+    defaultCssMapName = 'styles.css.map';
 
-cssDir = staticDir + '/css';
-lessDir = staticDir + '/less';
-
-defaultCss = cssDir + '/styles.css';
-defaultCssMin = cssDir + '/styles.min.css';
-defaultLess = lessDir + '/styles.less';
-
-defaultCssMapName = 'styles.css.map';
-
-require('load-grunt-config')(grunt, {
-  data: {
-    cssDir: cssDir,
-    defaultCss: defaultCss,
-    defaultCssMin: defaultCssMin,
-    defaultLess: defaultLess,
-    lessDir: lessDir,
-    staticDir: staticDir,
-  }
-});
+    require('load-grunt-config')(grunt, {
+        data: {
+            cssDir: cssDir,
+            defaultCss: defaultCss,
+            defaultCssMin: defaultCssMin,
+            defaultLess: defaultLess,
+            lessDir: lessDir,
+            staticDir: staticDir,
+        }
+    });
 };
