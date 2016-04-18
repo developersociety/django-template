282b1c3f6a30d284ff2abb8db9fe2abeaa9a23a3
Edvinas Jurevicius <ikonitas@gmail.com>
2016-04-18 11:25:23 +0100

    Add THUMBNAIL_DEBUG = True always debug thumbnail on local server

--- project_name/settings/local.py
+++ project_name/settings/local.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
 import os
 
@@ -6,6 +7,8 @@ from .base import *  # NOQA @UnusedWildImport
 
 
 DEBUG = True
+THUMBNAIL_DEBUG = True
+
 
 # Enable debugging for templates
 TEMPLATES = [
