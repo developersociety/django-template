4b4b9524caf4e3e9a53d77914a0746d003928a64
Alex Tomkins <tomkins@darkzone.net>
2017-01-05 13:21:20 +0000

    Make TEMPLATES more DRY for production

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/production.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/production.py
@@ -5,32 +5,14 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(' ')
 
 DEBUG = False
 
+
 # Use cached templates in production
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-        ],
-        'OPTIONS': {
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
-                'django.template.context_processors.request',
-                'django.contrib.messages.context_processors.messages',
-            ],
-            'loaders': [
-                ('django.template.loaders.cached.Loader', [
-                    'django.template.loaders.filesystem.Loader',
-                    'django.template.loaders.app_directories.Loader',
-                ]),
-            ],
-        },
-    },
+TEMPLATES[0]['APP_DIRS'] = False
+TEMPLATES[0]['OPTIONS']['loaders'] = [
+    ('django.template.loaders.cached.Loader', [
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    ]),
 ]
 
 
