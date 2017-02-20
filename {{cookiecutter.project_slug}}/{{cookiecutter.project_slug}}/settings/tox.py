1c242caa6ac7e6d54c29e35eb6d76f05344b556a
Alex Tomkins <tomkins@darkzone.net>
2017-02-20 10:30:59 +0000

    Add coverage reports
    Closes #43

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/tox.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/tox.py
@@ -18,3 +18,10 @@ STATIC_ROOT = os.environ['STATIC_ROOT']
 INSTALLED_APPS += [
     'django_extensions',
 ]
+
+
+# Test Runner
+# - Use XMLTestRunner for tox to output per test XML files
+# - Output these to a separate directory to avoid clutter
+TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
+TEST_OUTPUT_DIR = 'reports'
