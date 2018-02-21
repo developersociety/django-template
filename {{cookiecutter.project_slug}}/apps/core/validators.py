5d48b283f42b5bfdcf83dd28b6c0b4f59de150c0
Alex Tomkins <tomkins@darkzone.net>
2018-02-21 10:04:55 +0000

    Fir validators.py linting

--- {{cookiecutter.project_slug}}/apps/core/validators.py
+++ {{cookiecutter.project_slug}}/apps/core/validators.py
@@ -21,7 +21,6 @@ class ProhibitNullCharactersValidator:
 
     def __eq__(self, other):
         return (
-            isinstance(other, self.__class__) and
-            self.message == other.message and
+            isinstance(other, self.__class__) and self.message == other.message and
             self.code == other.code
         )
