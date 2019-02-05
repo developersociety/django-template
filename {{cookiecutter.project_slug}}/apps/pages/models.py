2d768546c61a71cc83e29f24d66eeeeac802c32f
Richard Smith <richardsmith@dev.ngo>
2019-02-05 12:31:25 +0000

    add language fields to homepage page model

--- {{cookiecutter.project_slug}}/apps/pages/models.py
+++ {{cookiecutter.project_slug}}/apps/pages/models.py
@@ -1,3 +1,7 @@
+{%- if cookiecutter.multilingual == 'y' %}
+from django.conf import settings
+{%- endif %}
+
 from wagtail.admin.edit_handlers import StreamFieldPanel
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
@@ -12,10 +16,39 @@ class StandardPage(Page):
         StreamFieldPanel('content'),
     ]
 
-
+{%- if cookiecutter.multilingual == 'y' %}
 class HomePage(Page):
     content = StreamField(BaseStreamBlock(), blank=True)
 
     content_panels = Page.content_panels + [
         StreamFieldPanel('content'),
     ]
+
+    language = models.CharField(
+        choices=settings.LANGUAGES,
+        max_length=5,
+        validators=[validate_language],
+        unique=True,
+    )
+
+    # only allow homepages to be added underneath root
+    # root page is only valid instance of bare page class
+    parent_page_types = ['wagtailcore.Page']
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('hero'),
+        FieldPanel('language'),
+        StreamFieldPanel('content'),
+    ]
+
+    search_fields = Page.search_fields + [
+        index.SearchField('content'),
+    ]
+{%- else %}
+class HomePage(Page):
+    content = StreamField(BaseStreamBlock(), blank=True)
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('content'),
+    ]
+{%- endif %}
