ad08fa898776f7f230f14316b7e1b7d26c7c515e
Richard Smith <richardsmith@dev.ngo>
2019-02-05 10:40:46 +0000

    fix tests

--- {{cookiecutter.project_slug}}/apps/core/tests/test_languages.py
+++ {{cookiecutter.project_slug}}/apps/core/tests/test_languages.py
@@ -3,11 +3,16 @@ from django.utils import translation
 
 class LanguageRedirectTest(TestCase):
 
-    def tearDown():
+    def tearDown(self):
         translation.deactivate()
     
     def test_english(self):
         response = self.client.get('/admin')
-
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/en/admin/', fetch_redirect_response=False)
+    
+    def test_dummy_translations(self):
+        response = self.client.get('/admin', HTTP_ACCEPT_LANGUAGE='uni')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/cy/', fetch_redirect_response=False)
+
