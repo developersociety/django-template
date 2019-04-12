0883dc42ab52d39c35fc719d22952e9c3325d7a0
Anna Hughes <anna@dev.ngo>
2019-04-12 15:12:30 +0100

    add accessibility styles

---
+++ {{cookiecutter.project_slug}}/static/js/main.js
@@ -0,0 +1,11 @@
+// Watch for user tabbing and enable focus styles if tab key pressed
+function handleFirstTab(e) {
+    if (e.keyCode === 9) {
+        // the "I am a keyboard user" key
+        document.body.classList.add('user-tabbing');
+        window.removeEventListener('keydown', handleFirstTab);
+    }
+}
+
+// Listen for scroll on mobile devices to show/hide menu
+window.addEventListener('keydown', handleFirstTab);
