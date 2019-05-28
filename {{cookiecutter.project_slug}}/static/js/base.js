8d4c557e3a1fdbbc8cc748f77d5273df69b56d8d
Anna Hughes <anna@dev.ngo>
2019-05-28 15:04:01 +0100

    lint

--- {{cookiecutter.project_slug}}/static/js/base.js
+++ {{cookiecutter.project_slug}}/static/js/base.js
@@ -1,9 +1,9 @@
-var main = document.querySelector('.main');
+var main = document.querySelector(".main");
 
 // Stretch main to fill height of screen minus header and footer
 if (main) {
-    var footer_height = document.querySelector('.footer').offsetHeight;
-    var header_height = document.querySelector('.header').offsetHeight;
+    var footer_height = document.querySelector(".footer").offsetHeight;
+    var header_height = document.querySelector(".header").offsetHeight;
     var total_height = header_height + footer_height;
-    main.style.minHeight = `${window.innerHeight - total_height}px`;
+    main.style.minHeight = window.innerHeight - total_height + "px";
 }
