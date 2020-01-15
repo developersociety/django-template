7271735a318e8a2bf40aa809dd3667b2cc85bad6
Richard Peters <richard@dev.ngo>
2020-01-15 15:40:02 +0000

    Double quote

--- {{cookiecutter.project_slug}}/project/settings/base.py
+++ {{cookiecutter.project_slug}}/project/settings/base.py
@@ -267,7 +267,7 @@ CSRF_COOKIE_HTTPONLY = True
 # Wagtail
 WAGTAIL_SITE_NAME = "{{ cookiecutter.project_name }}"
 BASE_URL = os.environ.get("WAGTAIL_BASE_URL", "")
-WAGTAILIMAGES_IMAGE_MODEL = 'images.CustomImage'
+WAGTAILIMAGES_IMAGE_MODEL = "images.CustomImage"
 WAGTAIL_ENABLE_UPDATE_CHECK = False
 WAGTAILSEARCH_BACKENDS = {
     "default": {
