a364bbedec55c1c927ad62322b042ecc3399c119
Alex Tomkins <tomkins@darkzone.net>
2025-07-12 15:14:43 +0100

    More defensive base.js

--- {{cookiecutter.project_slug}}/static/src/js/base.js
+++ {{cookiecutter.project_slug}}/static/src/js/base.js
@@ -2,8 +2,19 @@ const main = document.querySelector('.main');
 
 // Stretch main height to fill the screen
 if (main) {
-    const footer_height = document.querySelector('.footer').offsetHeight;
-    const header_height = document.querySelector('.header').offsetHeight;
+    let footer_height = '';
+    let header_height = '';
+    if (document.querySelector('.footer')) {
+        footer_height = document.querySelector('.footer').offsetHeight;
+    } else {
+        footer_height = '0';
+    }
+
+    if (document.querySelector('.header')) {
+        header_height = document.querySelector('.header').offsetHeight;
+    } else {
+        header_height = '0';
+    }
     const total_height = header_height + footer_height;
     main.style.minHeight = `${window.innerHeight - total_height}px`;
 }
